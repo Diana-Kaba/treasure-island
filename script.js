c05bca336248f093c45f86e1233eaab2af1e4dd6
Diana-Kaba <kabadiana327@gmail.com>
2022-12-09 17:00:02 +0200

    Third commit

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
+let map = document.getElementById("map");
 let treasure = {
-  x: Math.random() * 700,
-  y: Math.random() * 500,
+  x: Math.random() * map.width,
+  y: Math.random() * map.height,
 };
 
-let map = document.getElementById("map");
 let coords = document.getElementById("coords");
 map.onmousemove = showCoords;
 
