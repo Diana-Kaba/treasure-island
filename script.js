869b1a375ba5b4c30f4869fbd8ff09d07d8e61bb
Diana-Kaba <kabadiana327@gmail.com>
2022-12-12 18:21:56 +0200

    Circle2

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ function showCoords(event) {
   coords.value = `Координаты: ${x}, ${y}`;
   if (Math.abs(treasure.x - x) < 50 && Math.abs(treasure.y - y) < 50) {
     let treasureCircle = document.createElement("div");
-    treasureCircle.style = `border: 2px solid red; border-radius: 50%; width: 50px; height: 50px; position: "absolute"; top: treasure.x; left: treasure.y;`;
+    treasureCircle.style = `border: 2px solid red; border-radius: 50%; width: 50px; height: 50px; position: absolute; top: ${treasure.x}; left: ${treasure.y};`;
     wrap.appendChild(treasureCircle);
 
     alert("Сокровище здесь!");
